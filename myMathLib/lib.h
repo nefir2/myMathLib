6559aeae3c9e68f147a63ab8f15c5c6a0918ff24
nefir2 <vadim.ahtamov@gmail.com>
2022-06-16 15:14:32 +0300

    Добавьте файлы проекта.

---
+++ myMathLib/lib.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#ifdef MATHLIBRARY_EXPORTS
+#define MATHLIBRARY_API __declspec(dllexport)
+#else
+#define MATHLIBRARY_API __declspec(dllimport)
+#endif
+
+/// <summary>
+/// инициализация последовательности фибоначчи.
+/// </summary>
+/// <param name="a">F(n - 2)</param>
+/// <param name="b">F(n - 1)</param>
+/// <returns></returns>
+extern "C" MATHLIBRARY_API void fibonacci_init(const unsigned long long a, const unsigned long long b);
+
+/// <summary>
+/// производит следующее значение последовательности.
+/// </summary>
+/// <returns>
+/// возвращает true при успешном обновлении настоящего значения и индекса;
+/// возвращает false при переполнении и не меняет значение и индекс.
+/// </returns>
+extern "C" MATHLIBRARY_API bool fibonacci_next();
+
+/// <summary>
+/// возвращает нынешнее значение последовательности.
+/// </summary>
+/// <returns>настоящее значение последовательности.</returns>
+extern "C" MATHLIBRARY_API unsigned long long fibonacci_current();
+
+/// <summary>
+/// возвращает позицию настоящего значения в последовательности.
+/// </summary>
+/// <returns>
+/// настоящий индекс последовательности.
+/// </returns>
+extern "C" MATHLIBRARY_API unsigned fibonacci_index();
