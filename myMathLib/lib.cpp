80ad5b9612e96b837d6b92c9f1009f6e41cb11bb
nefir2 <vadim.ahtamov@gmail.com>
2022-06-17 09:37:13 +0300

    added realization of lib.h

---
+++ myMathLib/lib.cpp
@@ -0,0 +1,38 @@
+﻿#include "pch.h"
+#include <utility>
+#include <limits.h>
+#include "lib.h"
+#pragma region internal variables
+/// <summary>
+/// прошлое значение последовательности.
+/// </summary>
+static unsigned long long previuos;
+/// <summary>
+/// настоящее значение последовательности.
+/// </summary>
+static unsigned long long current;
+/// <summary>
+/// индекс значения последовательности.
+/// </summary>
+static unsigned index;
+#pragma endregion
+#pragma region functions
+void fibonacci_init(const unsigned long long a, const unsigned long long b) {
+	index = 0;
+	current = a;
+	previuos = b;
+}
+bool fibonacci_next() {
+	if (ULLONG_MAX - previuos < current || UINT_MAX == index) return false; //если выходит за пределы типа значения, то возвращается false.
+	if (index > 0) previuos += current; //получение следующего значения последовательности.
+	std::swap(current, previuos);
+	++index;
+	return true; //возврат true, при удачном получении следующего значения последовательности.
+}
+unsigned long long fibonacci_current() {
+	return current; //возврат настоящего числа.
+}
+unsigned fibonacci_index() {
+	return index; //возврат индекса настоящего числа последовательности.
+}
+#pragma endregion
